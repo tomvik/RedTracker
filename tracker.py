ab98e1995ba07b2ffe947412cce39c54a3ba8571
Tomas Lugo <ge52bar@mytum.de>
2019-12-19 14:25:34 +0100

    -Tracks the biggest object of a specific color -Update Journal

--- tracker.py
+++ tracker.py
@@ -6,11 +6,18 @@ import cv2
 # From a file, just write string in there.
 cap = cv2.VideoCapture(0)
 
+if not cap.isOpened():
+    print("Camera didn't open. Something went wrong")
+    exit
+
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 640*.7)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480*.7)
+
 # Define the codec and create VideoWriter object.
 fourcc = cv2.VideoWriter_fourcc(*'DIVX')
 # False the last one if it's going to be on gray scale.
 # The size of the window must be correct otherwise, trouble.
-out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640*2,480*2))
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
 
 # Take a picture and modify the filter values.
 ret, frame = cap.read()
@@ -18,41 +25,42 @@ window = ColorTrackBar.HSVTrackBar(frame)
 lower_values = window.showAndGetValues()
 print(lower_values)
 positions = [lower_values['H'], lower_values['S'], lower_values['V']]
-lower_bound_red = np.array([[lower_values['H'][0], lower_values['S'][0], lower_values['V'][0]],
+red_bounds = np.array([[lower_values['H'][0], lower_values['S'][0], lower_values['V'][0]],
                            [lower_values['H'][1], lower_values['S'][1], lower_values['V'][1]]])
-window = ColorTrackBar.HSVTrackBar(frame, positions)
-upper_values = window.showAndGetValues()
-print(upper_values)
-upper_bound_red = np.array([[upper_values['H'][0], upper_values['S'][0], upper_values['V'][0]],
-                           [upper_values['H'][1], upper_values['S'][1], upper_values['V'][1]]])
 
 while(cap.isOpened()):
     ret, frame = cap.read()
     # If ret is false, means there was an issue and there's no frame.
     if ret==True:
-        # Convert to gray scale.
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # Make the gray scale image have three channels.
-        gray_3_channel = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
-
-        # Concatenate on the horizontal axis the original frame and the gray one.
-        # Vertical axis: 0 instead of 1.
-        original_and_gray_horizontal = np.concatenate((frame, gray_3_channel), axis=1)
-
         # Convert to HSV scale, since with the H one we can get a better red filter.
         hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        circle_frame = frame.copy()
 
-        mask_1 = cv2.inRange(hsv_frame, lower_bound_red[0], lower_bound_red[1])
-        mask_2 = cv2.inRange(hsv_frame, upper_bound_red[0], upper_bound_red[1])
-
-        mask_hor = np.concatenate((mask_1, mask_2), axis=1)
-
-        mask = cv2.bitwise_or(mask_1, mask_2)
+        mask = cv2.inRange(hsv_frame, red_bounds[0], red_bounds[1])
         red_frame = cv2.bitwise_and(frame, frame, mask=mask)
+
+        contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        if len(contours) > 0:
+            max_contour = max(contours, key=cv2.contourArea)
+            ((x,y), r) = cv2.minEnclosingCircle(max_contour)
+            if r > 2:
+                M = cv2.moments(max_contour)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+                # draw the circle and centroid on the frame,
+                # then update the list of tracked points
+                cv2.circle(circle_frame, (int(x), int(y)), int(r),(0, 255, 255), 2)
+                cv2.circle(circle_frame, center, 3, (0, 0, 255), -1)
+                cv2.putText(circle_frame,"centroid", (center[0]+10,center[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.4,(0, 0, 255),1)
+                cv2.putText(circle_frame,"("+str(center[0])+","+str(center[1])+")", (center[0]+10,center[1]+15), cv2.FONT_HERSHEY_SIMPLEX, 0.4,(0, 0, 255),1)
+
         mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
 
         red_hor = np.concatenate((mask, red_frame), axis=1)
-        final_complete = np.concatenate((original_and_gray_horizontal, red_hor), axis=0)
+        # Concatenate on the horizontal axis the original frame and the gray one.
+        # Vertical axis: 0 instead of 1.
+        original_and_circle = np.concatenate((frame, circle_frame), axis=1)
+        final_complete = np.concatenate((original_and_circle, red_hor), axis=0)
 
 
         # Show the frame on the screen.
